4861a87c3d6646045cd97b76132865e41a27b394
kiffykowboy <chris@Chriss-MacBook-Pro.local>
2021-03-15 00:35:58 -0500

    contact commit

---
+++ contact/scripts.js
@@ -0,0 +1,16 @@
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
+
+
+
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
